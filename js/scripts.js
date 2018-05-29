770e2d797242b1ce475424748e59b29495836b48
Steven Colburn and William Kinzig <william@>
2018-05-29 09:38:28 -0700

    Updating files after practicing javascript

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ function divide(number1,number2)
 {
 	return (number1/number2);
 }
-/*
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
@@ -28,9 +28,22 @@ var resultDivide = divide(number1,number2);
 
 alert("Add: "+resultAdd+" Subtract: " +resultSubtract+
 " Multiply: "+resultMultiply+" Divide: "+resultDivide);
-*/
 
 
+/*
 var string1 = "This is a test.";
 var string2 = string1.replace(/test/i,"calculator");
 alert(string2);
+*/
+
+/*
+function calculateBMI(mass,height)
+{
+  //mass in pounds, height in inches
+  return ((mass/(height*height))*703);
+}
+
+var mass = parseInt(prompt("Enter your weight (lbs):"));
+var height = parseInt(prompt("Enter your height (inches):"));
+alert("Your Body Mass Index is: " + calculateBMI(mass,height));
+*/
