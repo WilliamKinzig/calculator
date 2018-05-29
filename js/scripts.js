70b598f174dcacc08e57bbee3159408b0c3b7602
Steven Colburn and William Kinzig <steven@>
2018-05-29 09:01:27 -0700

    Testing regex

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ function divide(number1,number2)
 {
 	return (number1/number2);
 }
-
+/*
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
@@ -28,3 +28,9 @@ var resultDivide = divide(number1,number2);
 
 alert("Add: "+resultAdd+" Subtract: " +resultSubtract+
 " Multiply: "+resultMultiply+" Divide: "+resultDivide);
+*/
+
+
+var string1 = "This is a test.";
+var string2 = string1.replace(/test/i,"calculator");
+alert(string2);
