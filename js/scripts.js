2cde20da2bb750a0e2edbfd92087f7d34c25b650
Steven Colburn and William Kinzig <william@>
2018-05-29 15:01:03 -0700

    Adding buttons to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -18,32 +18,36 @@ function divide(number1,number2)
 	return (number1/number2);
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-var resultAdd = add(number1, number2);
-var resultSubtract = subtract(number1,number2);
-var resultMultiply = multiply(number1,number2);
-var resultDivide = divide(number1,number2);
-
-alert("Add: "+resultAdd+" Subtract: " +resultSubtract+
-" Multiply: "+resultMultiply+" Divide: "+resultDivide);
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var resultAdd = add(number1, number2);
+// var resultSubtract = subtract(number1,number2);
+// var resultMultiply = multiply(number1,number2);
+// var resultDivide = divide(number1,number2);
+// alert("Add: "+resultAdd+" Subtract: " +resultSubtract+
+// " Multiply: "+resultMultiply+" Divide: "+resultDivide);
 
 
-/*
-var string1 = "This is a test.";
-var string2 = string1.replace(/test/i,"calculator");
-alert(string2);
-*/
-
-/*
-function calculateBMI(mass,height)
+$(function()
 {
-  //mass in pounds, height in inches
-  return ((mass/(height*height))*703);
-}
+  $("#btn-add").click(function()
+  {
+    
+  });
 
-var mass = parseInt(prompt("Enter your weight (lbs):"));
-var height = parseInt(prompt("Enter your height (inches):"));
-alert("Your Body Mass Index is: " + calculateBMI(mass,height));
-*/
+  // $("p#paragraph1").click(function()
+  // {
+  //   var currentParagraph = $("#paragraph1");
+  //   var currentClass = "show-border";
+  //   if(currentParagraph.hasClass(currentClass))
+  //   {
+  //     currentParagraph.removeClass(currentClass);
+  //   }
+  //   else
+  //   {
+  //     currentParagraph.removeClass();
+  //     currentParagraph.addClass(currentClass);
+  //   }
+  // });
+
+});
