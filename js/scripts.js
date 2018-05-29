ad3854489b04669ade8b662c4789a90e5302f60e
Steven Colburn and William Kinzig <steven@>
2018-05-29 15:48:37 -0700

    add.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-// function add(number1,number2)
-// {
-// 	return (number1+number2);
-// }
-//
-// function subtract(number1,number2)
-// {
-// 	return (number1-number2);
-// }
-//
-// function multiply(number1,number2)
-// {
-// 	return (number1*number2);
-// }
-//
-// function divide(number1,number2)
-// {
-// 	return (number1/number2);
-// }
+function add(number1,number2)
+{
+	return (number1+number2);
+}
+
+function subtract(number1,number2)
+{
+	return (number1-number2);
+}
+
+function multiply(number1,number2)
+{
+	return (number1*number2);
+}
+
+function divide(number1,number2)
+{
+	return (number1/number2);
+}
 
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
@@ -30,12 +30,13 @@
 
 $(function()
 {
-  $("button#btn-add").click(function()
+  $("#btn-add").click(function()
   {
-		// var test = 10;
-		// var number1 = parseInt($("#number1"));
-		// var string1 = test.toString();
-		$("#testVal").val("this is a test.");
+
+		var number1 = parseInt($("#number1").val());
+		var number2 = parseInt($("#number2").val());
+		var returnValue = add(number1,number2);
+		$("#returnValue").val(returnValue);
   });
 
 });
